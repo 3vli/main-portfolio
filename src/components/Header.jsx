30d05bbda3abc1737b87a59a20a9590f830fc5ad
3vli <om1ak2345@gmail.com>
2025-01-01 11:37:52 +0200

    fix header icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,9 +9,11 @@ const Header = () => {
            <h1 className='text-3xl '>Ali</h1>
         </div>
         <div className='flex flex-shrink-0 justify-center gap-4 text-2xl'>
-           <FaInstagram className='cursor-pointer ' />
-           <FaGithub    className='cursor-pointer ' />
-           <FaDiscord className='cursor-pointer ' />
+           <a href="https://www.instagram.com/3vli13/" target='_blank'><FaInstagram /></a>
+           <a href="https://github.com/3vli" target='_blank'><FaGithub  /></a>
+           <a href="https://discord.com/users/804842497243086938" target='_blank'><FaDiscord /></a>
+           
+           
         </div>
        
       </nav>
